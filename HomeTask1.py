345d82641e6fcc94d1dc3046ad7c7b43c544530b
“Vladyslav <unconfigured@null.spigotmc.org>
2025-02-26 11:34:49 +0200

    Home task 1 - corrections

--- HomeTask1.py
+++ HomeTask1.py
@@ -6,30 +6,37 @@ import random
 
 # Create list of 100 random numbers from 0 to 1000
 ls = [random.randint(0, 1000) for x in range(100)]
-lsSorted = []
+ls_sorted = []
 print(ls)
 
 # Sort list from min to max(without using sort())
 # I've decided to choose selection sort O(n^2) because it is simple and works well on small lists
 while len(ls) > 0:
     # Select and remove minimum element from ls and append to lsSorted
-    lsSorted.append(ls.pop(ls.index(min(ls))))
+    ls_sorted.append(ls.pop(ls.index(min(ls))))
 
 print('ls:', ls)
-print('lsSorted:', lsSorted)
+print('lsSorted:', ls_sorted)
 
 ''' Calculate average for even and odd numbers
 print both average result in console '''
 # Variables' initialization
-evenSum = 0; evenCount = 0; oddSum = 0; oddCount = 0
-for i in range(len(lsSorted)):
-    currentNumber = lsSorted[i]
-    if currentNumber % 2 == 0:
-        evenSum += currentNumber
-        evenCount += 1
+even_sum = even_count = odd_sum = odd_count = 0
+for i in range(len(ls_sorted)):
+    current_number = ls_sorted[i]
+    if current_number % 2 == 0:
+        even_sum += current_number
+        even_count += 1
     else:
-        oddSum += currentNumber
-        oddCount += 1
+        odd_sum += current_number
+        odd_count += 1
 
-print('Average for even numbers', evenSum / evenCount)
-print('Average for odd numbers', oddSum / oddCount)
+if even_count != 0:
+    print('Average for even numbers', even_sum / even_count)
+else:
+    print('No even numbers')
+
+if odd_count != 0:
+    print('Average for odd numbers', odd_sum / odd_count)
+else:
+    print('No odd numbers')
