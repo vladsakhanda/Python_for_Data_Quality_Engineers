6e0c554f953f0b3b219180031fe7abc0325f86e8
“Vladyslav <unconfigured@null.spigotmc.org>
2025-03-07 17:27:53 +0200

    HomeTask5 branch is created.

--- Functions.py
+++ Functions.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def str_to_date_format(date: str, date_format: str):
     """
-    Takes date and converts to taken data_format.
+    Takes date and converts to taken data_format
     If date is in incorrect format, the function returns: None
     :param date: str
     :return: datetime
