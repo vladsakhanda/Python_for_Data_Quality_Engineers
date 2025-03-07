bc58b2b11cc64206b600713c4f71bc93dc7a0045
“Vladyslav <unconfigured@null.spigotmc.org>
2025-03-07 17:32:27 +0200

    HomeTask5 branch is created.

---
+++ Functions.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+def str_to_date_format(date: str, date_format: str):
+    """
+    Takes date and converts to taken data_format
+    If date is in incorrect format, the function returns: None
+    :param date: str
+    :return: datetime
+    """
+    try:
+        return datetime.strptime(date, date_format)
+    except ValueError or TypeError:
+        return None
+
