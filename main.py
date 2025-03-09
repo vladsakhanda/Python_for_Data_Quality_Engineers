479cf20e266d7d35f80be9be79f211a625bc7cf7
“Vladyslav <unconfigured@null.spigotmc.org>
2025-03-09 16:37:21 +0200

    HomeTask7 branch is created.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from Functions import str_to_date_format
-from classes.LuckyNumber import LuckyNumber
-from classes.News import News
-from classes.PrivateAd import PrivateAd
-
-objects = []
+from classes.Feeds import Feeds
+from classes.FileProcessor import FileProcessor, DEFAULT_FILE
 
 def exit():
-    if input('Type \'E\' if you want to exit. Type anything if you want to add something else: ') == 'E':
+    if input('Type \'E\' if you want to exit. Type anything if you want to add something else: ').upper() == 'E':
         print()
         return True
     else:
@@ -20,46 +17,50 @@ while not is_exit:
 1. News
 2. Private Ad
 3. Lucky Number
-4. News feed
+4. Show news feed
     
 You choose: ''')
 
     print('---\n')
     if user_input in ('1', 'News', '1. News'):
-        news = News(input('You chose News! Enter news text: '),
-                    input('Enter your city: '))
+        user_text = input('You chose News! Enter news text: ')
+        city = input('Enter your city: ')
+        news = Feeds.News(user_text, city)
         print('\nThe next news were added:', news, end='\n\n')
 
-        objects.append(news)
+        # objects.append(news)
+        FileProcessor.append_to_file(DEFAULT_FILE, str(news))
 
         is_exit = exit()
-
     elif user_input in ('2', 'Private Ad', '2. Private Ad'):
         text = input('You chose Private Ad! Enter private ad text: ')
         expiration_date = None
 
         while expiration_date is None:
-            expiration_date = str_to_date_format(input('Enter expiration date in format "%d/%m/%Y": '),
-                                                 PrivateAd.DATETIME_FORMAT)
+            date = input('Enter expiration date in format "%d/%m/%Y": ')
+            expiration_date = str_to_date_format(date, Feeds.PrivateAd.DATETIME_FORMAT)
             print(expiration_date)
-            if expiration_date is not None:
-                private_ad = PrivateAd(text, expiration_date)
+            if expiration_date:
+                private_ad = Feeds.PrivateAd(text, expiration_date)
                 print('\nThe next private ad were added:', private_ad, end='\n\n')
-                objects.append(private_ad)
+                # objects.append(private_ad)
+                FileProcessor.append_to_file(DEFAULT_FILE, str(private_ad))
 
                 is_exit = exit()
             else:
                 print('\nYour date is in the incorrect format.\n\n')
     elif user_input in ('3', 'Lucky Number', '3. Lucky Number'):
-        lucky_number = LuckyNumber(input('You chose Lucky Number! Enter your name: '))
+        name = input('You chose Lucky Number! Enter your name: ')
+        lucky_number = Feeds.LuckyNumber(name)
         print('\nThe next lucky number were added:', lucky_number, end='\n\n')
 
-        objects.append(lucky_number)
+        # objects.append(lucky_number)
+        FileProcessor.append_to_file(DEFAULT_FILE, str(lucky_number))
 
         is_exit = exit()
-    elif user_input in ('4', 'News feed', '4. News eed'):
+    elif user_input in ('4', 'Show news feed', '4. Show news feed'):
         print('News feed:\n---')
-        [print(object) for object in objects]
+        print(FileProcessor.read_from_file(DEFAULT_FILE))
         print('---')
 
         is_exit = exit()
