b9ef07eef060d9919a449b489ede084edefb0610
“Vladyslav <unconfigured@null.spigotmc.org>
2025-03-10 23:05:54 +0200

    HomeTask6 - made necessary corrections.

--- Readme.md
+++ Readme.md
@@ -3,6 +3,7 @@
 Expand previous Homework 5 with additional class, which allow to provide records by text file:
 
 1.Define your input format (one or many records)
+
 2.Default folder or user provided file path
 
 3.Remove file if it was successfully processed
--- classes/Feeds.py
+++ classes/Feeds.py
@@ -1,24 +1,52 @@
 import random
+import re
 from datetime import datetime
 
 DATETIME_FORMAT = "%d/%m/%Y %H.%M"
 
 
+def is_valid_feed(line):
+    return (re.match(Feeds.NEWS_PATTERN, line) or
+            re.match(Feeds.LUCKY_NUMBER_PATTERN, line) or
+            re.match(Feeds.PRIVATE_AD_PATTERN, line))
+
+
+def create_feed_instance(line):
+    if re.match(Feeds.NEWS_PATTERN, line):
+        match = re.match(Feeds.NEWS_PATTERN, line)
+        text, city, date = match.groups()
+        return Feeds.News(text=text, city=city, date=date)
+    elif re.match(Feeds.LUCKY_NUMBER_PATTERN, line):
+        match = re.match(Feeds.LUCKY_NUMBER_PATTERN, line)
+        name, lucky_number = match.groups()
+        return Feeds.LuckyNumber(name=name, lucky_number=lucky_number)
+    elif re.match(Feeds.PRIVATE_AD_PATTERN, line):
+        match = re.match(Feeds.PRIVATE_AD_PATTERN, line)
+        text, expiration_date_str = match.groups()
+        expiration_date = datetime.strptime(expiration_date_str, "%d/%m/%Y")
+        return Feeds.PrivateAd(text=text, expiration_date=expiration_date)
+    return None
+
+
 class Feeds:
+    DATE_FORMAT = "%d/%m/%Y %H.%M"
+    NEWS_PATTERN = r"^News:\s(.+?),\s(.+?),\s(\d{2}/\d{2}/\d{4}\s\d{2}\.\d{2})$"
+    LUCKY_NUMBER_PATTERN = r"^Lucky Number:\s(.+?),\syour lucky number is (\d+)!$"
+    PRIVATE_AD_PATTERN = r"^Private Ad:\s(.+?),\suntil:\s(\d{2}/\d{2}/\d{4}),\s-?\d+\sdays left$"
 
     class LuckyNumber:
-        def __init__(self, name: str):
+        def __init__(self, name: str, lucky_number=None):
             self.name = name
-            self.lucky_number = random.randint(0, 100)
+            self.lucky_number = lucky_number if lucky_number else random.randint(0, 100)
 
         def __str__(self):
             return f"Lucky Number: {self.name}, your lucky number is {self.lucky_number}!"
 
     class News:
-        def __init__(self, text: str, city: str):
+        def __init__(self, text: str, city: str, date=None):
             self.text = text
             self.city = city
-            self.date = datetime.now().strftime(DATETIME_FORMAT)
+            self.date = date if date else datetime.now().strftime(DATETIME_FORMAT)
 
         def __str__(self):
             return f"News: {self.text}, {self.city}, {self.date} "
