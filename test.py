123d58f336806fc59c1ce6358f1bdc03a31970c6
“Vladyslav <unconfigured@null.spigotmc.org>
2025-03-17 21:04:48 +0200

    HomeTask8 - made necessary changes

--- test.py
+++
@@ -1,9 +0,0 @@
-from classes.Feeds import Feeds
-
-# Імпортуємо та тестуємо JSONFileImporter
-json_importer = Feeds.JSONFileImporter('feeds.json')
-imported_feeds = json_importer.import_feeds()
-
-# Виводимо імпортовані записи
-for feed in imported_feeds:
-    print(feed)
